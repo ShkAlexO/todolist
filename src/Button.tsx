db5d8491be0dec0406c4c8cc8c264599ac268b93
ShkAlexO <shk.alex.o@gmail.com>
2025-08-18 17:03:50 +0300

    Удаление компоненты

--- src/Button.tsx
+++
@@ -1,9 +0,0 @@
-type Props = {
-  title: string
-  className?: string
-  onClick?: () => void
-}
-
-export const Button = ({ title, className, onClick }: Props) => {
-  return <button className={className} onClick={onClick}>{title}</button>
-}
