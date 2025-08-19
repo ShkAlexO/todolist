0bb38cfbb2912a0afcf1c758d2b84f002cad64fc
ShkAlexO <shk.alex.o@gmail.com>
2025-08-19 16:29:57 +0300

    подключение тестов

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// MY-NOTE-7
+import { defineConfig } from 'vitest/config'; // Импорт функции для конфигурации Vitest
+import react from '@vitejs/plugin-react-swc'; // Плагин для работы с React через SWC (быстрее Babel)
+import { resolve } from 'path'; // Утилита Node.js для работы с путями
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react()], // Плагин React оставили без изменений
+  test: {
+    globals: true, // Позволяет использовать функции тестирования (test, expect) глобально, без импорта
+    environment: 'jsdom', // Имитация браузерной среды для тестов React
+    setupFiles: resolve(__dirname, 'src/setupTests.ts'), // Указываем файл, который выполняется перед всеми тестами (например, для настройки jest-dom)
+  },
+});
+
