2e6ca6bcdc42af097b8fd82a470ba29c1b5380e9
ShkAlexO <shk.alex.o@gmail.com>
2025-08-28 18:18:10 +0300

    мелкие правки

--- src/AddItemForm.tsx
+++ src/AddItemForm.tsx
@@ -1,18 +1,17 @@
 import IconButton from "@mui/material/IconButton";
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline'
 import TextField from "@mui/material/TextField";
-import {type ChangeEvent, type KeyboardEvent, useState} from "react";
+import {type ChangeEvent, type KeyboardEvent, memo, useState} from "react";
 
 export type AddItemFormPropsType = {
   addItem: (title: string) => void;
 }
 
-export const AddItemForm = ({addItem}: AddItemFormPropsType) => {
+export const AddItemForm = memo(({addItem}: AddItemFormPropsType) => {
   const [newTaskTitle, setNewTaskTitle] = useState('')
   const [error, setError] = useState<string | null>(null)
 
   const onNewTitleChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    setError('')
     setNewTaskTitle(e.currentTarget.value)
   }
 
@@ -26,12 +25,12 @@ export const AddItemForm = ({addItem}: AddItemFormPropsType) => {
   }
 
   const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (error !== null) {
+      setError(null)
+    }
     if (e.key === 'Enter' && newTaskTitle.trim().length !== 0) {
       addItem(newTaskTitle)
       setNewTaskTitle('')
-    }
-    else {
-      setError('Title is required')
     }
   }
 
@@ -46,7 +45,7 @@ export const AddItemForm = ({addItem}: AddItemFormPropsType) => {
         onChange={onNewTitleChangeHandler}
         helperText={error}
         error={!!error}
-        sx={{marginRight: '5px' }}
+        sx={{marginRight: '5px'}}
       />
 
       <IconButton
@@ -58,4 +57,4 @@ export const AddItemForm = ({addItem}: AddItemFormPropsType) => {
       </IconButton>
     </div>
   )
-}
+})
